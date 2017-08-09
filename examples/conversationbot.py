af05fa25f66bca34dff86568e1cbbf26d4d37713
MRM <mr.mashoufi.95@gmail.com>
2017-08-09 13:03:44 +0430

    change reply_text to sendMessage
    Some times reply_text don't work but sendMessage will work at all

--- examples/conversationbot.py
+++ examples/conversationbot.py
@@ -34,8 +34,8 @@ GENDER, PHOTO, LOCATION, BIO = range(4)
 
 def start(bot, update):
     reply_keyboard = [['Boy', 'Girl', 'Other']]
-
-    update.message.reply_text(
+    
+    bot.sendMessage(update.message.chat_id,
         'Hi! My name is Professor Bot. I will hold a conversation with you. '
         'Send /cancel to stop talking to me.\n\n'
         'Are you a boy or a girl?',
@@ -47,7 +47,7 @@ def start(bot, update):
 def gender(bot, update):
     user = update.message.from_user
     logger.info("Gender of %s: %s" % (user.first_name, update.message.text))
-    update.message.reply_text('I see! Please send me a photo of yourself, '
+    bot.sendMessage(update.message.chat_id,'I see! Please send me a photo of yourself, '
                               'so I know what you look like, or send /skip if you don\'t want to.',
                               reply_markup=ReplyKeyboardRemove())
 
@@ -59,7 +59,7 @@ def photo(bot, update):
     photo_file = bot.get_file(update.message.photo[-1].file_id)
     photo_file.download('user_photo.jpg')
     logger.info("Photo of %s: %s" % (user.first_name, 'user_photo.jpg'))
-    update.message.reply_text('Gorgeous! Now, send me your location please, '
+    bot.sendMessage(update.message.chat_id,'Gorgeous! Now, send me your location please, '
                               'or send /skip if you don\'t want to.')
 
     return LOCATION
@@ -68,7 +68,7 @@ def photo(bot, update):
 def skip_photo(bot, update):
     user = update.message.from_user
     logger.info("User %s did not send a photo." % user.first_name)
-    update.message.reply_text('I bet you look great! Now, send me your location please, '
+    bot.sendMessage(update.message.chat_id,'I bet you look great! Now, send me your location please, '
                               'or send /skip.')
 
     return LOCATION
@@ -79,7 +79,7 @@ def location(bot, update):
     user_location = update.message.location
     logger.info("Location of %s: %f / %f"
                 % (user.first_name, user_location.latitude, user_location.longitude))
-    update.message.reply_text('Maybe I can visit you sometime! '
+    bot.sendMessage(update.message.chat_id,'Maybe I can visit you sometime! '
                               'At last, tell me something about yourself.')
 
     return BIO
@@ -88,7 +88,7 @@ def location(bot, update):
 def skip_location(bot, update):
     user = update.message.from_user
     logger.info("User %s did not send a location." % user.first_name)
-    update.message.reply_text('You seem a bit paranoid! '
+    bot.sendMessage(update.message.chat_id,'You seem a bit paranoid! '
                               'At last, tell me something about yourself.')
 
     return BIO
@@ -97,7 +97,7 @@ def skip_location(bot, update):
 def bio(bot, update):
     user = update.message.from_user
     logger.info("Bio of %s: %s" % (user.first_name, update.message.text))
-    update.message.reply_text('Thank you! I hope we can talk again some day.')
+    bot.sendMessage(update.message.chat_id,'Thank you! I hope we can talk again some day.')
 
     return ConversationHandler.END
 
@@ -105,7 +105,7 @@ def bio(bot, update):
 def cancel(bot, update):
     user = update.message.from_user
     logger.info("User %s canceled the conversation." % user.first_name)
-    update.message.reply_text('Bye! I hope we can talk again some day.',
+    bot.sendMessage(update.message.chat_id,'Bye! I hope we can talk again some day.',
                               reply_markup=ReplyKeyboardRemove())
 
     return ConversationHandler.END
